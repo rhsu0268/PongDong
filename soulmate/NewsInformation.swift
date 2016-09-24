93c3f4e7840ceeeed8ed647b8a5bb253369dfee6
Richard Hsu <rhsu0268@gmail.com>
2016-09-23 22:08:51 -0400

    Added a NewsInfromation class

---
+++ soulmate/NewsInformation.swift
@@ -0,0 +1,14 @@
+//
+//  NewsInformation.swift
+//  soulmate
+//
+//  Created by Richard Hsu on 9/23/16.
+//  Copyright © 2016 Richard Hsu. All rights reserved.
+//
+
+import Foundation
+
+class NewsInformation: NSObject
+{
+    
+}
