4794fe517ac56fc8ebc1dc98c056e9df45b8b572
Richard Hsu <rhsu0268@gmail.com>
2016-10-10 23:48:31 -0400

    Added JSONSerialization code

--- soulmate/APIClient.swift
+++ soulmate/APIClient.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+public let RCHNetworkingErrorDomain = "com.rch.soleLife.NetworkingError"
+public let MissingHTTPResponseError: Int = 10
+
 typealias JSON = [String : AnyObject]
 typealias JSONTaskCompletion = (JSON?, NSHTTPURLResponse?, NSError?) -> Void
 typealias JSONTask = NSURLSessionDataTask
@@ -47,4 +50,61 @@ protocol APIClient
     // 2nd closure:
     func fetch<T>(request: NSURLRequest, parse: JSON -> T?, completion: APIResult <T> -> Void)
 
+}
+
+extension APIClient
+{
+    func JSONTaskWithRequest(request: NSURLRequest, completion: JSONTaskCompletion) -> JSONTask
+    {
+        // create a task
+        
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            
+            
+            // guard statement
+            // let - force cast
+            guard let HTTPResponse = response as? NSHTTPURLResponse
+                
+            else
+            {
+                let userInfo = [
+                    NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")
+                ]
+                
+                let error = NSError(domain: RCHNetworkingErrorDomain, code: MissingHTTPResponseError, userInfo: userInfo)
+                
+                completion(nil, nil, error)
+                
+                // transfers control and hands it to JSONTaskWithRequest
+                return
+            }
+            
+            
+            if data == nil
+            {
+                if let error = error
+                {
+                    completion(nil, HTTPResponse, error)
+                }
+            }
+            else
+            {
+                // convert data to JSON object and return it
+                switch HTTPResponse.statusCode
+                {
+                    case 200:
+                        do
+                        {
+                            let json = try NSJSONSerialization.JSONObjectWithData(data!, options: [])
+                        }
+                    
+                }
+            }
+            
+            
+        }
+        return task
+        // convert data to JSONdata
+        
+    }
 }
