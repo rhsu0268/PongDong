cca715dd8ae142d4e988270d1824adf0fd0871c0
Richard Hsu <rhsu0268@gmail.com>
2016-10-22 22:13:06 -0400

    Added APIClient

--- soulmate/APIClient.swift
+++ soulmate/APIClient.swift
@@ -2,118 +2,99 @@
 //  APIClient.swift
 //  soulmate
 //
-//  Created by Richard Hsu on 10/5/16.
+//  Created by Richard Hsu on 10/22/16.
 //  Copyright © 2016 Richard Hsu. All rights reserved.
 //
 
 import Foundation
 
-public let TRENetworkingErrorDomain = "com.treehouse.Stormy.NetworkingError"
-public let MissingHTTPResponseError: Int = 10
+public let TRENetworkingErrorDomain = "com.richardhsu.Soulmate.NetworkingError"
+public let MissingHttpResponseError: Int = 10
 public let UnexpectedResponseError: Int = 20
 
 typealias JSON = [String : AnyObject]
 typealias JSONTaskCompletion = (JSON?, NSHTTPURLResponse?, NSError?) -> Void
 typealias JSONTask = NSURLSessionDataTask
 
-enum APIResult<T> {
+enum APIResult<T>
+{
     case Success(T)
     case Failure(ErrorType)
 }
 
-protocol JSONDecodable {
+protocol JSONDecodable
+{
     init?(JSON: [String : AnyObject])
 }
 
-protocol Endpoint {
+protocol Endpoint
+{
     var baseURL: NSURL { get }
     var path: String { get }
     var request: NSURLRequest { get }
+
 }
 
-protocol APIClient {
+protocol APIClient
+{
     var configuration: NSURLSessionConfiguration { get }
     var session: NSURLSession { get }
     
     func JSONTaskWithRequest(request: NSURLRequest, completion: JSONTaskCompletion) -> JSONTask
-    func fetch<T: JSONDecodable>(request: NSURLRequest, parse: JSON -> T?, completion: APIResult<T> -> Void)
+    func fetch<T: JSONDecodable>(request: NSURLRequest, parse: JSON -> T?, completion: APIResult <T> -> Void)
+
 }
 
-extension APIClient {
-    func JSONTaskWithRequest(request: NSURLRequest, completion: JSONTaskCompletion) -> JSONTask {
-        
-        let task = session.dataTaskWithRequest(request) { data, response, error in
+extension APIClient
+{
+    func JSONTaskWithRequest(request: NSURLRequest, completion: JSONTaskCompletion) -> JSONTask
+    {
+        let task = session.dataTaskWithRequest(request)
+        {
+            data, response, error in
             
-            guard let HTTPResponse = response as? NSHTTPURLResponse else {
+            guard let HTTPResponse = response as? NSHTTPURLResponse else
+            {
                 let userInfo = [
+                    
                     NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")
+                    
                 ]
                 
-                let error = NSError(domain: TRENetworkingErrorDomain, code: MissingHTTPResponseError, userInfo: userInfo)
+                let error = NSError(domain: TRENetworkingErrorDomain, code: MissingHttpResponseError, userInfo: userInfo)
+                
+                
                 completion(nil, nil, error)
+                
                 return
             }
             
-            if data == nil {
-                if let error = error {
+            if data == nil
+            {
+                if let error = error
+                {
                     completion(nil, HTTPResponse, error)
                 }
-            } else {
-                switch HTTPResponse.statusCode {
-                case 200:
-                    do {
-                        let json = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String : AnyObject]
-                        completion(json, HTTPResponse, nil)
-                    } catch let error as NSError {
-                        completion(nil, HTTPResponse, error)
-                    }
-                default: print("Received HTTP Response: \(HTTPResponse.statusCode) - not handled")
+            }
+            else
+            {
+                switch HTTPResponse.statusCode
+                {
+                    case 200:
+                        do
+                        {
+                            let json = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as?
+                            [String : AnyObject]
+                            completion(json, HTTPResponse, nil)
+                        }
+                        catch let error as NSError
+                        {
+                            completion(nil, HTTPResponse, error)
+                        }
+                        default: print("Received HTTP Response: \(HTTPResponse.statusCode) - not handled")
                 }
             }
         }
-        
         return task
     }
-    
-    func fetch<T>(request: NSURLRequest, parse: JSON -> T?, completion: APIResult<T> -> Void) {
-        
-        let task = JSONTaskWithRequest(request) { json, response, error in
-            
-            dispatch_async(dispatch_get_main_queue()) {
-                guard let json = json else {
-                    if let error = error {
-                        completion(.Failure(error))
-                    } else {
-                        // TODO: Implement Error Handling
-                    }
-                    return
-                }
-                
-                if let value = parse(json) {
-                    completion(.Success(value))
-                } else {
-                    let error = NSError(domain: TRENetworkingErrorDomain, code: UnexpectedResponseError, userInfo: nil)
-                    completion(.Failure(error))
-                }
-            }
-        }
-        
-        task.resume()
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
