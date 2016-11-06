12bf3efc743693ffc944d71e6e2e23e937be4277
Richard Hsu <rhsu0268@gmail.com>
2016-11-06 15:10:03 -0500

    Took out code that was never used

--- soulmate/ViewController.swift
+++ soulmate/ViewController.swift
@@ -32,10 +32,6 @@ class ViewController: UIViewController {
         
         
         // start network request to alchemyapi
-        let session = NSURLSession.sharedSession()
-        
-        let url = NSURL(string: "https://access.alchemyapi.com/calls/data/GetNews?apikey=0c946bde49878224025230853ec995cc1693dc3e&return=enriched.url.title,enriched.url.url&start=1473897600&end=1474585200&q.enriched.url.cleanedTitle=charlotte&q.enriched.url.enrichedTitle.docSentiment.type=negative&q.enriched.url.enrichedTitle.taxonomy.taxonomy_.label=society&count=25&outputMode=json")
-        
         newsAPIClient.fetchCurrentNews()
         {
             result in
