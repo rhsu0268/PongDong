de2debaba33f4222967b38a62f3ee8fdc4c0615f
Richard Hsu <rhsu0268@gmail.com>
2016-11-08 15:39:09 -0500

    Changed NewsArticle model

--- soulmate/NewsArticle.swift
+++ soulmate/NewsArticle.swift
@@ -11,15 +11,23 @@ import Foundation
 
 class NewsArticle: NSObject
 {
+    var author: String
+    var newsDescription: String
+    var publishedAt: String
     var title: String
     var url: String
+    var urlToImage: String
     
  
  
-    init (title: String, url: String)
+    init (author: String, descripton: String, publishedAt: String, title: String, url: String, urlToImage: String)
     {
+        self.author = author
+        self.newsDescription = description
+        self.publishedAt = publishedAt
         self.title = title
         self.url = url
+        self.urlToImage = urlToImage
     }
  
 }
