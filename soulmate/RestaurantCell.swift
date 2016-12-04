05aad37175c7ed8ef639189bf04b34d39cbd0ea0
Richard Hsu <rhsu0268@gmail.com>
2016-12-03 22:30:06 -0500

    Added restaurantCell

---
+++ soulmate/RestaurantCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantCell.swift
+//  soulmate
+//
+//  Created by Richard Hsu on 12/3/16.
+//  Copyright © 2016 Richard Hsu. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
