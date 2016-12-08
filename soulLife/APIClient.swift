5b400a0ee9e8925dc56ad1b2b53e884520c1474a
Richard Hsu <rhsu0268@gmail.com>
2016-12-07 21:32:47 -0500

    Added APIClient

---
+++ soulLife/APIClient.swift
@@ -0,0 +1,56 @@
+//
+//  APIClient.swift
+//  soulLife
+//
+//  Created by Richard Hsu on 12/7/16.
+//  Copyright © 2016 Richard Hsu. All rights reserved.
+//
+
+import Foundation
+
+
+public let soulLifeNetworkingErrorDomain = "com.souLife.NetworkingError"
+
+public let MissingHTTPResponseError: Int = 10
+public let UnexpectedResponseError: Int = 20
+
+protocol JSONDecodable
+{
+    init?(JSON: [String : AnyObject])
+}
+
+protocol Endpoint
+{
+    var baseURL: String { get }
+    var path: String { get }
+    var parameters: [String : AnyObject] { get }
+}
+
+extension Endpoint
+{
+    var queryComponents: [URLQueryItem]
+    {
+        var components = [URLQueryItem]()
+        
+        for (key, value) in parameters
+        {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.append(queryItem)
+        }
+        
+        return components
+    }
+    
+    var request: URLRequest
+    {
+        var components = URLComponents(string: baseURL)!
+        
+        components.path = path
+        components.queryItems = queryComponents
+        
+        
+        let url = components.url!
+        
+        return URLRequest(url: url)
+    }
+}
---
+++ soulLife/RootController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  soulLife
+//
+//  Created by Richard Hsu on 12/7/16.
+//  Copyright © 2016 Richard Hsu. All rights reserved.
+//
+
+import UIKit
+
+class RootController: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
