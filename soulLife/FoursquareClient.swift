592990cb155235d06fe9e91e28015bb402520658
Richard Hsu <rhsu0268@gmail.com>
2016-12-29 18:23:18 -0500

    Added FoursquareModels

---
+++ soulLife/FoursquareClient.swift
@@ -0,0 +1,21 @@
+//
+//  FoursquareClient.swift
+//  soulLife
+//
+//  Created by Richard Hsu on 12/29/16.
+//  Copyright © 2016 Richard Hsu. All rights reserved.
+//
+
+import Foundation
+
+/*
+enum FourSquare: Endpoint
+{
+    case Venues(VenueEndpoint)
+    
+    enum VenueEndpoint: Endpoint
+    {
+        case Searc
+    }
+}
+*/ 
