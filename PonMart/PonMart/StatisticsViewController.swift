d1771dfbb8b938ecb046832dd87c487698040389
Richard Hsu <rhsu0268@gmail.com>
2017-01-21 15:26:33 -0500

    Successfully added barchartview

--- PonMart/PonMart/StatisticsViewController.swift
+++ PonMart/PonMart/StatisticsViewController.swift
@@ -13,19 +13,43 @@ class StatisticsViewController: UIViewController {
 
     
     @IBOutlet var statsView: BarChartView!
-   
+    
+    var months: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        statsView.noDataText = "You have no sales!"
+        //statsView.noDataText = "You have no sales!"
+        
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(dataPoints: months, values: unitsSold)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setChart(dataPoints: [String], values: [Double])
+    {
+        statsView.noDataText = "You have no data!"
+        
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count
+        {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(dataSets: [chartDataSet])
+        statsView.data = chartData
     }
     
 
