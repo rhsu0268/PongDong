a8402308b1d037ec14d20684311e7a6d2f8cf90e
Richard Hsu <rhsu0268@gmail.com>
2017-02-12 22:07:34 -0500

    Successfully pulled in array to tableView within view

---
+++ PonMart/PonMart/UserItemCell.swift
@@ -0,0 +1,28 @@
+//
+//  UserItemCell.swift
+//  PonMart
+//
+//  Created by Richard Hsu on 2/12/17.
+//  Copyright © 2017 Richard Hsu. All rights reserved.
+//
+
+import UIKit
+
+class UserItemCell: UITableViewCell {
+
+    
+    @IBOutlet var itemName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PonMart/PonMart/ViewItemViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewItemViewController.swift
+//  PonMart
+//
+//  Created by Richard Hsu on 2/12/17.
+//  Copyright © 2017 Richard Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewItemViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+
+    
+    var items: [String] = ["We", "Heart", "Swift"]
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(3)
+        return self.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UserItemCell
+        
+        let item = items[indexPath.row]
+        print(item)
+        cell.itemName.text = item
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //
+    }
+
+}
