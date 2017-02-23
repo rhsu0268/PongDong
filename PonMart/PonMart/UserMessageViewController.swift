0194b60e9b5d43b1688a25b246d9e91b94e5968a
Richard Hsu <rhsu0268@gmail.com>
2017-02-23 16:00:24 -0500

    Saving nil for profile image

--- PonMart/PonMart/UserMessageViewController.swift
+++ PonMart/PonMart/UserMessageViewController.swift
@@ -15,10 +15,13 @@ class UserMessageViewController: UIViewController, UITableViewDelegate, UITableV
     
     var items: [String] = ["We", "Heart", "Swift"]
     
+    var users: [User] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        fetchChatUsers()
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,15 +46,17 @@ class UserMessageViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count
+        return self.users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! UserCell
         
-        let user = items[indexPath.row]
-        print(user)
-        cell.userName.text = user
+        let user = users[indexPath.row]
+        //print(user)
+        print(user.userImage)
+        print(user.userEmail)
+        cell.userName.text = user.userEmail
         
         return cell
     }
@@ -67,34 +72,45 @@ class UserMessageViewController: UIViewController, UITableViewDelegate, UITableV
             {
                 
                 
-                let publicItem = PublicItem()
-                publicItem.userId = dictionary["userId"] as! String
+                let user = User()
+                //publicItem.userId = dictionary["userId"] as! String
                 
-                if uid != publicItem.userId
-                {
+                //if uid != publicItem.userId
+                //{
                     
                     // crashes if the key does not match those in firebase
-                    publicItem.name = dictionary["itemName"] as! String
-                    publicItem.itemDescription = dictionary["itemDescription"] as! String
-                    publicItem.type = dictionary["itemType"] as! String
-                    publicItem.condition = dictionary["itemCondition"] as! String
-                    publicItem.price = Double(dictionary["itemPrice"] as! String)!
-                    publicItem.itemImageUrl = dictionary["itemImageUrl"] as! String
+                    user.userEmail = dictionary["email"] as! String
+                
+                
+                    // get the image if it exists
+                    if let image = dictionary["userImage"] as? String
+                    {
+                        user.userImage = image
+                    }
+                    else
+                    {
+                        user.userImage = "nil"
+                    }
+                    //publicItem.itemDescription = dictionary["itemDescription"] as! String
+                    //publicItem.type = dictionary["itemType"] as! String
+                    //publicItem.condition = dictionary["itemCondition"] as! String
+                    //publicItem.price = Double(dictionary["itemPrice"] as! String)!
+                    //publicItem.itemImageUrl = dictionary["itemImageUrl"] as! String
                     
-                    publicItem.createdDate = dictionary["createdDate"] as! String
+                    //publicItem.createdDate = dictionary["createdDate"] as! String
                     
-                    print(publicItem)
+                    //print(publicItem)
                     
-                    self.publicItems.append(publicItem)
+                    self.users.append(user)
                     
-                    print(self.publicItems)
+                    //print(self.publicItems)
                     DispatchQueue.main.async(execute: {
                         
                         self.tableView.reloadData()
                     })
                 }
                 
-            }
+            //}
             
             print(snapshot)
             
